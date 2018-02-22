fa92b4fd03ea75ee9131dedac402ec6ffe5e5c8b
Navdeep Kumar <navdeepk@hytech.in>
2018-02-22 12:57:41 +0530

    Added files.

---
+++ contracts/Owned.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.11;
+
+contract Owned{
+  address owner;
+
+  modifier onlyOwner()
+  {
+    require(msg.sender==owner);
+    _;
+  }
+
+  function Owned()
+  {
+    owner=msg.sender;
+  }
+
+}
